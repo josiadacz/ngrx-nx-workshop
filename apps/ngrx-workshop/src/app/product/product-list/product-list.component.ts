7cdb6cc6df500a9c0530571d8464e0a04f9ccb14
Alex Okrushko <alex@ts.dev>
2022-10-04 09:53:22 -0400

    m11: Call state

--- apps/ngrx-workshop/src/app/product/product-list/product-list.component.ts
+++ apps/ngrx-workshop/src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { map, Observable, shareReplay } from 'rxjs';
 
+<<<<<<< HEAD
 import { BasicProduct, ProductRating } from '@ngrx-nx-workshop/api-interfaces';
 import { Store } from '@ngrx/store';
 <<<<<<< HEAD
@@ -12,14 +13,29 @@ import { Dictionary } from '@ngrx/entity';
 =======
 import { ProductModel } from '../../model/product';
 import { selectProducts } from '../product.selectors';
+=======
+import { Rating } from '@ngrx-nx-workshop/api-interfaces';
+import { RatingService } from '../rating.service';
+
+import { createSelector, Store } from '@ngrx/store';
+import { LoadingState } from '../../shared/call-state';
+import { selectProducts, selectProductsCallState } from '../product.selectors';
+>>>>>>> 6006bb3 (m11: Call state)
 import * as actions from './actions';
 >>>>>>> bf5c9ab (m8: Combining selectors)
+
+const productListVm = createSelector(
+  selectProducts,
+  selectProductsCallState,
+  (products, productsCallState) => ({ products, productsCallState })
+);
 
 @Component({
   selector: 'ngrx-nx-workshop-home',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
+<<<<<<< HEAD
 export class ProductListComponent {
   products$: Observable<BasicProduct[] | undefined> = this.store.select(
     selectors.getProducts
@@ -32,6 +48,16 @@ export class ProductListComponent {
   // Make LoadingState be available in the template.
   readonly LoadingState = LoadingState;
 =======
+=======
+export class ProductListComponent implements OnInit {
+  productListVm$ = this.store.select(productListVm);
+
+  customerRatings$?: Observable<{ [productId: string]: Rating }>;
+
+  // Make LoadingState be available in the template.
+  readonly LoadingState = LoadingState;
+
+>>>>>>> 6006bb3 (m11: Call state)
   constructor(
     private readonly store: Store,
     private readonly ratingService: RatingService
